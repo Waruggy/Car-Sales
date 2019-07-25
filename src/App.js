556a44263a887728893771b0ab2ccc49a2d9b8f4
Charles <cwang8413@yahoo.com>
2019-07-25 12:19:09 -0700

    Got it now.

--- src/App.js
+++ src/App.js
@@ -4,45 +4,42 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { connect } from 'react-redux';
+import { addCar, remove } from './store/actions';
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    store: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
-
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
+const App = ({ additionalPrice, car, store, addCar, remove }) => {
+ 
+  const removeFeature = feature => {
+    console.log(feature)
+    remove(feature)   
   };
 
   const buyItem = item => {
-    // dipsatch an action here to add an item
+    addCar(item)
   };
-
+    
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={car} />
+        <AddedFeatures car={car}  removeFeature={removeFeature}/>
       </div>
       <div className="box">
-        <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures store={store}  butItem={buyItem}/>
+        <Total car={car} additionalPrice={additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = ({ additionalPrice, car, store}) => {
+  console.log(additionalPrice)
+
+   return {
+    additionalPrice: additionalPrice,
+    car: car,
+    store: store
+  }
+}
+
+ export default connect(mapStateToProps, { addCar, remove })(App)
